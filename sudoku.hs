a8fe1751a378f8fa7d98a88d149ffe5027f0966a
Ethan <riley787cmplex@gmail.com>
2018-07-01 12:41:05 +0100

    more grid stuffs

--- sudoku.hs
+++ sudoku.hs
@@ -1,4 +1,5 @@
 import qualified Data.Map as Map
+import Data.Maybe
 
 type Row = Map.Map Int Char
 type Grid = Map.Map Int Row
@@ -11,3 +12,18 @@ makeEmptyGrid size =
 makeEmptySudokuGrid :: Grid
 makeEmptySudokuGrid = makeEmptyGrid 9
 
+digits = ['1'..'9']
+
+insertToGrid :: Int -> Int -> Char -> Grid -> Grid
+insertToGrid row col digit grid 
+    | elem digit digits = Map.insert row (Map.insert col digit $ fromJust $ Map.lookup row grid) grid
+    | otherwise = error "not a digit"
+
+flattenHelper :: [(Int, Map.Map Int a)] -> [(Int, a)] 
+flattenHelper [x] = Map.toList(snd x)
+flattenHelper (x:xs) = Map.toList (snd x) ++ flattenHelper xs
+
+flatten :: Map.Map Int (Map.Map Int a) -> [a]
+flatten map = snd $ unzip $ flattenHelper $ Map.toList map
+
+get3by3 :: Int -> Int -> Grid -> String
