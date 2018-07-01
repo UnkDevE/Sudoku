e1adb87f1ce06deca56aa84572a496d539aa45ee
Ethan <riley787cmplex@gmail.com>
2018-07-01 18:04:09 +0100

    add valid

--- sudoku.hs
+++ sudoku.hs
@@ -1,5 +1,8 @@
 import qualified Data.Map as Map
+import Data.Bifunctor (bimap)
 import Data.Maybe
+import Data.Monoid
+import Control.Monad (join)
 
 type Row = Map.Map Int Char
 type Grid = Map.Map Int Row
@@ -26,4 +29,34 @@ flattenHelper (x:xs) = Map.toList (snd x) ++ flattenHelper xs
 flatten :: Map.Map Int (Map.Map Int a) -> [a]
 flatten map = snd $ unzip $ flattenHelper $ Map.toList map
 
-get3by3 :: Int -> Int -> Grid -> String
+flattenRow :: Map.Map Int a -> [a]
+flattenRow map = snd $ unzip $ Map.toList map
+
+gridSize :: Grid -> Int
+gridSize grid = length $ Map.toList grid
+
+get3by3Helper :: Int -> (Map.Map Int a) -> [a]
+get3by3Helper row grid = snd $ unzip 
+    $ filter (\a -> ((fst a)-1) `div` 3 == (row-1)) $ Map.toList grid
+
+get3by3 :: (Int, Int) -> Grid -> String
+get3by3 (row, col) grid = foldr (\x acc -> get3by3Helper col x ++ acc) [] $ get3by3Helper row grid
+
+getRow :: Int -> Grid -> String
+getRow row grid = flattenRow $ fromJust $ Map.lookup row grid 
+
+getCol :: Int -> Grid -> String
+getCol col grid = 
+    foldr (\x acc -> if (fst x) `rem` 9 == col then (snd x):acc else acc) [] $ zip [1..] $ flatten grid 
+
+find3by3 :: (Int, Int) -> (Int, Int)
+find3by3 pos = join bimap (`rem` 3) pos 
+
+isInGrid :: (Int, Int) -> Grid -> Bool
+isInGrid pos grid =
+    '.' /= fromJust $ Map.lookup (snd pos) $ fromJust $ Map.lookup (fst pos) grid
+
+vaild :: Char -> (Int, Int) -> Grid -> Bool
+vaild digit pos grid = 
+    not $ getAny $ mconcat $ map (Any . (elem digit)) 
+        [get3by3 (find3by3 pos) grid, getRow (fst pos) grid, getCol (snd pos) grid]  
