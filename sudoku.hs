bbbc0d6a5ae28c1dd43fb9e5fe9dfb9d6b38ce77
Ethan <riley787cmplex@gmail.com>
2018-07-14 17:40:35 +0100

    add testGrid

--- sudoku.hs
+++ sudoku.hs
@@ -86,6 +86,52 @@ unflatten :: Int -> String -> Grid
 unflatten size flatgrid =
     Map.fromList $ zip [1..] $ map (Map.fromList . zip [1..]) $ chunksOf size flatgrid
 
+insertManyToGrid :: [((Int, Int), Char)] -> Grid -> Grid
+insertManyToGrid xs grid = 
+    foldl (\grid item -> insertToGrid (fst item) (snd item) grid) grid xs 
+
+testGrid :: Grid
+testGrid = insertManyToGrid 
+    [
+        ((1,2), '8'),
+        ((1,5), '5'),
+        ((1,6), '1'),
+        ((1,9), '3'),
+        ((2,3), '7'),
+        ((2,5), '2'),
+        ((2,7), '4'),
+        ((2,8), '5'),
+        ((3,1), '5'),
+        ((3,7), '8'),
+        ((3,8), '1'),
+        ((4,3), '1'),
+        ((4,4), '7'),
+        ((4,6), '3'),
+        ((4,7), '2'),
+        ((4,8), '9'),
+        ((4,9), '8'),
+        ((5,3), '8'),
+        ((5,7), '3'),
+        ((6,1), '7'),
+        ((6,2), '3'),
+        ((6,3), '6'),
+        ((6,4), '2'),
+        ((6,6), '8'),
+        ((6,7), '5'),
+        ((7,2), '7'),
+        ((7,3), '5'),
+        ((7,9), '4'),
+        ((8,2), '9'),
+        ((8,3), '3'),
+        ((8,5), '8'),
+        ((8,7), '6'),
+        ((9,1), '8'),
+        ((9,4), '6'),
+        ((9,5), '1'),
+        ((9,8), '3')
+    ] 
+    $ makeEmptySudokuGrid
+
 digits :: [Char]
 digits = ['1'..'9']
 
